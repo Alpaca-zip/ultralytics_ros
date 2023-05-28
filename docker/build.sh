ceb72ca3da004282e15eeb463cd8b7ba2ea2b448
alpaca-zip <zip.lottestr@gmail.com>
2023-05-28 23:25:59 +0900

    initial commit for melodic-devel

--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
-docker buildx build --load -t ultralytics_ros:noetic .
--- docker/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 6080:80 --shm-size=512m ultralytics_ros:noetic
