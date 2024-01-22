9f5f54d3e280159f90e135d6a9c0db65803a6791
Alpaca-zip <alpaca.zip@gmail.com>
2024-01-22 13:36:03 +0900

    Add functions to publish segmentation masks, and update parameter
    Remove scripts that explicitly destroy node

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ultralytics_ros [![ROS2-humble Industrial CI](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/humble-ci.yml/badge.svg)](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/humble-ci.yml) [![ROS2-humble Docker Build Check](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/humble-docker-build-check.yml/badge.svg)](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/humble-docker-build-check.yml)
-ROS2 package for real-time object detection using the Ultralytics YOLO, enabling flexible integration with various robotics applications.
+ROS 2 package for real-time object detection using the Ultralytics YOLO, enabling flexible integration with various robotics applications.
 
 |  `tracker_node`  |  `tracker_with_cloud_node`  |
 | :------------: | :-----------------------: |
 | <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/9da7dbbf-5cc0-41bc-be82-d481abbf552a" width="450px"> | **TODO** |
 
-- The `tracker_node` provides real-time object detection on incoming ROS image messages using the Ultralytics YOLO model.
+- The `tracker_node` provides real-time object detection on incoming ROS 2 image messages using the Ultralytics YOLO model.
 
 ## Setup ⚙
 ```
@@ -24,37 +24,51 @@ $ ros2 launch ultralytics_ros tracker.launch.xml debug:=true
 ```
 ## `tracker_node`
 ### Params
+### Params
 - `yolo_model`: Pre-trained Weights.  
-For yolov8, you can choose `yolov8*.pt`, `yolov8*-seg.pt`, `yolov8*-pose.pt`.
+For yolov8, you can choose `yolov8*.pt`, `yolov8*-seg.pt`.
 
   |  YOLOv8  |  <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/08770080-bf20-470b-8269-eee7a7c41acc" width="350px">  |
   | :-------------: | :-------------: |
   |  **YOLOv8-seg**  |  <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/7bb6650c-769d-41c1-86f7-39fcbf01bc7c" width="350px">  |
-  |  **YOLOv8-pose**  |  <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/46d2a5ef-193b-4f83-a0b3-6cc0d5a3756c" width="350px">  |
   
   See also: https://docs.ultralytics.com/models/
-- `publish_rate`: Publish rate for `image_topic`.
-- `image_topic`: Topic name for image.
-- `detection_topic`: Topic name for 2D bounding box.
+- `input_topic`: Topic name for input image.
+- `result_topic`: Topic name of the custom message containing the 2D bounding box and the mask image.
+- `result_image_topic`: Topic name of the image on which the detection and segmentation results are plotted.
 - `conf_thres`: Confidence threshold below which boxes will be filtered out.
 - `iou_thres`: IoU threshold below which boxes will be filtered out during NMS.
 - `max_det`: Maximum number of boxes to keep after NMS.
 - `tracker`: Tracking algorithms.
-- `classes`: List of class indices to consider.  
-See also: https://github.com/ultralytics/ultralytics/blob/main/ultralytics/datasets/coco128.yaml 
-- `debug`:  If true, run simple viewer.
-- `debug_conf`:  Whether to plot the detection confidence score.
-- `debug_line_width`: Line width of the bounding boxes.
-- `debug_font_size`: Font size of the text.
-- `debug_labels`: Font to use for the text.
-- `debug_font`: Whether to plot the label of bounding boxes.
-- `debug_boxes`: Whether to plot the bounding boxes.
+- `device`: Device to run the model on(e.g. cpu or cuda:0).
+  ```xml
+  <arg name="device" default="cpu"/>
+  ```
+  ```xml
+  <arg name="device" default="cuda:0"/>
+  ```
+- `classes`: List of class indices to consider.
+  ```xml
+  <param name="classes" value="0, 1" value-sep=", "/> <!-- person, bicycle -->
+  ```
+  See also: https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/datasets/coco128.yaml 
+- `result_conf`:  Whether to plot the detection confidence score.
+- `result_line_width`: Line width of the bounding boxes.
+- `result_font_size`: Font size of the text.
+- `result_labels`: Font to use for the text.
+- `result_font`: Whether to plot the label of bounding boxes.
+- `result_boxes`: Whether to plot the bounding boxes.
 ### Topics
 - Subscribed Topics:
-  - Image data from `image_topic` parameter. ([sensor_msgs/Image](https://docs.ros.org/en/api/sensor_msgs/html/msg/Image.html))
+  - Image data from `input_topic` parameter. ([sensor_msgs/Image](https://github.com/ros2/common_interfaces/blob/humble/sensor_msgs/msg/Image.msg))
 - Published Topics:
-  - Debug images to `/debug_image` topic. ([sensor_msgs/Image](https://docs.ros.org/en/api/sensor_msgs/html/msg/Image.html))
-  - Detected objects(2D bounding box) to `detection_topic` parameter. (vision_msgs/Detection2DArray)
+  - Plotted images to `result_image_topic` parameter. ([sensor_msgs/Image](https://github.com/ros2/common_interfaces/blob/humble/sensor_msgs/msg/Image.msg))
+  - Detected objects(2D bounding box, mask image) to `result_topic` parameter. (ultralytics_ros/YoloResult)
+    ```
+    std_msgs/Header header
+    vision_msgs/Detection2DArray detections
+    sensor_msgs/Image[] masks
+    ```
 ## `tracker_with_cloud_node`
 **TODO**
 ## Docker with KITTI datasets 🐳
@@ -65,7 +79,7 @@ See also: https://github.com/ultralytics/ultralytics/blob/main/ultralytics/datas
 $ docker pull alpacazip/ultralytics_ros:humble
 $ docker run -p 6080:80 --shm-size=512m alpacazip/ultralytics_ros:humble
 ```
-### Run tracker_node & tracker_with_cloud_node
+### Run tracker_node
 ```
 $ ros2 launch ultralytics_ros kitti_tracker.launch.xml
 $ cd ~/colcon_ws/src/ultralytics_ros/ros2bag && ros2 bag play kitti_2011_09_26_drive_0106_synced --clock --loop
--- script/tracker_node.py
+++ script/tracker_node.py
@@ -17,144 +17,161 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import cv_bridge
+import numpy as np
 import rclpy
 from ament_index_python.packages import get_package_share_directory
-from cv_bridge import CvBridge
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from ultralytics import YOLO
 from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
+from ultralytics_ros.msg import YoloResult
 
 
 class TrackerNode(Node):
     def __init__(self):
         super().__init__("tracker_node")
         self.declare_parameter("yolo_model", "yolov8n.pt")
-        self.declare_parameter("publish_rate", 10)
-        self.declare_parameter("detection_topic", "detection_result")
-        self.declare_parameter("image_topic", "image_raw")
+        self.declare_parameter("input_topic", "image_raw")
+        self.declare_parameter("result_topic", "yolo_result")
+        self.declare_parameter("result_image_topic", "yolo_image")
         self.declare_parameter("conf_thres", 0.25)
         self.declare_parameter("iou_thres", 0.45)
         self.declare_parameter("max_det", 300)
         self.declare_parameter("classes", list(range(80)))
         self.declare_parameter("tracker", "bytetrack.yaml")
-        self.declare_parameter("debug", False)
-        self.declare_parameter("debug_conf", True)
-        self.declare_parameter("debug_line_width", 1)
-        self.declare_parameter("debug_font_size", 1)
-        self.declare_parameter("debug_font", "Arial.ttf")
-        self.declare_parameter("debug_labels", True)
-        self.declare_parameter("debug_boxes", True)
-        yolo_model = self.get_parameter("yolo_model").get_parameter_value().string_value
-        publish_rate = (
-            self.get_parameter("publish_rate").get_parameter_value().integer_value
-        )
-        detection_topic = (
-            self.get_parameter("detection_topic").get_parameter_value().string_value
-        )
-        image_topic = (
-            self.get_parameter("image_topic").get_parameter_value().string_value
-        )
-        self.max_det = self.get_parameter("max_det").get_parameter_value().integer_value
-        self.conf_thres = (
-            self.get_parameter("conf_thres").get_parameter_value().double_value
-        )
-        self.iou_thres = (
-            self.get_parameter("iou_thres").get_parameter_value().double_value
-        )
-        self.classes = (
-            self.get_parameter("classes").get_parameter_value().integer_array_value
-        )
-        self.tracker = self.get_parameter("tracker").get_parameter_value().string_value
-        self.debug = self.get_parameter("debug").get_parameter_value().bool_value
-        self.debug_conf = (
-            self.get_parameter("debug_conf").get_parameter_value().bool_value
-        )
-        self.debug_line_width = (
-            self.get_parameter("debug_line_width").get_parameter_value().integer_value
-        )
-        self.debug_font_size = (
-            self.get_parameter("debug_font_size").get_parameter_value().integer_value
-        )
-        self.debug_font = (
-            self.get_parameter("debug_font").get_parameter_value().string_value
-        )
-        self.debug_labels = (
-            self.get_parameter("debug_labels").get_parameter_value().bool_value
-        )
-        self.debug_boxes = (
-            self.get_parameter("debug_boxes").get_parameter_value().bool_value
-        )
-        self.bridge = CvBridge()
+        self.declare_parameter("device", "cpu")
+        self.declare_parameter("result_conf", True)
+        self.declare_parameter("result_line_width", 1)
+        self.declare_parameter("result_font_size", 1)
+        self.declare_parameter("result_font", "Arial.ttf")
+        self.declare_parameter("result_labels", True)
+        self.declare_parameter("result_boxes", True)
+
         path = get_package_share_directory("ultralytics_ros")
+        yolo_model = self.get_parameter("yolo_model").get_parameter_value().string_value
         self.model = YOLO(f"{path}/models/{yolo_model}")
-        self.timer_image = self.create_timer(0.1, self.publish_debug_image)
-        self.timer_detection = self.create_timer(
-            1.0 / publish_rate, self.publish_detection
+        self.model.fuse()
+
+        self.bridge = cv_bridge.CvBridge()
+        self.use_segmentation = yolo_model.endswith("-seg.pt")
+
+        input_topic = (
+            self.get_parameter("input_topic").get_parameter_value().string_value
         )
-        self.header = None
-        self.results = None
-        self.create_subscription(Image, image_topic, self.image_callback, 1)
-        self.image_pub = self.create_publisher(Image, "debug_image", 1)
-        self.detection_pub = self.create_publisher(Detection2DArray, detection_topic, 1)
-
-    def publish_debug_image(self):
-        if self.debug and self.results is not None:
-            plotted_image = self.results[0].plot(
-                conf=self.debug_conf,
-                line_width=self.debug_line_width,
-                font_size=self.debug_font_size,
-                font=self.debug_font,
-                labels=self.debug_labels,
-                boxes=self.debug_boxes,
-            )
-            debug_image_msg = self.bridge.cv2_to_imgmsg(plotted_image, encoding="bgr8")
-            self.image_pub.publish(debug_image_msg)
-
-    def publish_detection(self):
-        if self.results is not None:
-            detections_msg = Detection2DArray()
-            detections_msg.header = self.header
-            bounding_box = self.results[0].boxes.xywh
-            classes = self.results[0].boxes.cls
-            confidence_score = self.results[0].boxes.conf
-            for bbox, cls, conf in zip(bounding_box, classes, confidence_score):
-                detection = Detection2D()
-                detection.bbox.center.position.x = float(bbox[0])
-                detection.bbox.center.position.y = float(bbox[1])
-                detection.bbox.size_x = float(bbox[2])
-                detection.bbox.size_y = float(bbox[3])
-                hypothesis = ObjectHypothesisWithPose()
-                hypothesis.hypothesis.class_id = self.results[0].names.get(int(cls))
-                hypothesis.hypothesis.score = float(conf)
-                detection.results.append(hypothesis)
-                detections_msg.detections.append(detection)
-            self.detection_pub.publish(detections_msg)
+        result_topic = (
+            self.get_parameter("result_topic").get_parameter_value().string_value
+        )
+        result_image_topic = (
+            self.get_parameter("result_image_topic").get_parameter_value().string_value
+        )
+        self.create_subscription(Image, input_topic, self.image_callback, 1)
+        self.results_pub = self.create_publisher(YoloResult, result_topic, 1)
+        self.result_image_pub = self.create_publisher(Image, result_image_topic, 1)
 
     def image_callback(self, msg):
-        self.header = msg.header
-        numpy_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        self.results = self.model.track(
-            source=numpy_image,
-            conf=self.conf_thres,
-            iou=self.iou_thres,
-            max_det=self.max_det,
-            classes=self.classes,
-            tracker=self.tracker,
-            verbose=False,
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        conf_thres = self.get_parameter("conf_thres").get_parameter_value().double_value
+        iou_thres = self.get_parameter("iou_thres").get_parameter_value().double_value
+        max_det = self.get_parameter("max_det").get_parameter_value().integer_value
+        classes = (
+            self.get_parameter("classes").get_parameter_value().integer_array_value
         )
+        tracker = self.get_parameter("tracker").get_parameter_value().string_value
+        device = self.get_parameter("device").get_parameter_value().string_value
+        results = self.model.track(
+            source=cv_image,
+            conf=conf_thres,
+            iou=iou_thres,
+            max_det=max_det,
+            classes=classes,
+            tracker=tracker,
+            device=device,
+            verbose=False,
+            retina_masks=True,
+        )
+
+        if results is not None:
+            yolo_result_msg = YoloResult()
+            yolo_result_image_msg = Image()
+            yolo_result_msg.header = msg.header
+            yolo_result_image_msg.header = msg.header
+            yolo_result_msg.detections = self.create_detections_array(results)
+            yolo_result_image_msg = self.create_result_image(results)
+            if self.use_segmentation:
+                yolo_result_msg.masks = self.create_segmentation_masks(results)
+            self.results_pub.publish(yolo_result_msg)
+            self.result_image_pub.publish(yolo_result_image_msg)
+
+    def create_detections_array(self, results):
+        detections_msg = Detection2DArray()
+        bounding_box = results[0].boxes.xywh
+        classes = results[0].boxes.cls
+        confidence_score = results[0].boxes.conf
+        for bbox, cls, conf in zip(bounding_box, classes, confidence_score):
+            detection = Detection2D()
+            detection.bbox.center.position.x = float(bbox[0])
+            detection.bbox.center.position.y = float(bbox[1])
+            detection.bbox.size_x = float(bbox[2])
+            detection.bbox.size_y = float(bbox[3])
+            hypothesis = ObjectHypothesisWithPose()
+            hypothesis.hypothesis.class_id = results[0].names.get(int(cls))
+            hypothesis.hypothesis.score = float(conf)
+            detection.results.append(hypothesis)
+            detections_msg.detections.append(detection)
+        return detections_msg
+
+    def create_result_image(self, results):
+        result_conf = self.get_parameter("result_conf").get_parameter_value().bool_value
+        result_line_width = (
+            self.get_parameter("result_line_width").get_parameter_value().integer_value
+        )
+        result_font_size = (
+            self.get_parameter("result_font_size").get_parameter_value().integer_value
+        )
+        result_font = (
+            self.get_parameter("result_font").get_parameter_value().string_value
+        )
+        result_labels = (
+            self.get_parameter("result_labels").get_parameter_value().bool_value
+        )
+        result_boxes = (
+            self.get_parameter("result_boxes").get_parameter_value().bool_value
+        )
+        plotted_image = results[0].plot(
+            conf=result_conf,
+            line_width=result_line_width,
+            font_size=result_font_size,
+            font=result_font,
+            labels=result_labels,
+            boxes=result_boxes,
+        )
+        result_image_msg = self.bridge.cv2_to_imgmsg(plotted_image, encoding="bgr8")
+        return result_image_msg
+
+    def create_segmentation_masks(self, results):
+        masks_msg = []
+        for result in results:
+            if hasattr(result, "masks") and result.masks is not None:
+                for mask_tensor in result.masks:
+                    mask_numpy = (
+                        np.squeeze(mask_tensor.data.to("cpu").detach().numpy()).astype(
+                            np.uint8
+                        )
+                        * 255
+                    )
+                    mask_image_msg = self.bridge.cv2_to_imgmsg(
+                        mask_numpy, encoding="mono8"
+                    )
+                    masks_msg.append(mask_image_msg)
+        return masks_msg
 
 
 def main(args=None):
-    try:
-        rclpy.init(args=args)
-        node = TrackerNode()
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+    rclpy.init(args=args)
+    node = TrackerNode()
+    rclpy.spin(node)
 
 
 if __name__ == "__main__":
